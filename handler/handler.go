7455c2f4cdb8ee50bc40f7a3fd4195c12e3d6651
Haruki Ido <rikuharu3705@outlook.jp>
2022-05-02 15:45:30 +0900

    fix handler

--- handler/handler.go
+++ handler/handler.go
@@ -1,13 +1,22 @@
 package handler
 
 import (
+	"embed"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"mime"
 	"net/http"
+	"path"
+	"path/filepath"
 	"sync"
 
 	"github.com/HarukiIdo/go_sample/model"
 	"github.com/HarukiIdo/go_sample/service"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type CommentHandler struct {
@@ -53,3 +62,59 @@ func (h *CommentHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// go:embed vite-project/dist
+var assets embed.FS
+
+func tryRead(requestedPath string, w http.ResponseWriter) error {
+	log.Println(requestedPath)
+	f, err := assets.Open(path.Join("vite-project/dist", requestedPath))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// ディレクトリならエラー
+	stat, _ := f.Stat()
+	if stat.IsDir() {
+		return errors.New("path is dir")
+	}
+
+	// MIMEタイプを設定
+	ext := filepath.Ext(requestedPath)
+	var contentType string
+	if m := mime.TypeByExtension(ext); m != "" {
+		contentType = m
+	} else {
+		contentType = "application/octet-stream"
+	}
+	w.Header().Set("Content-Type", contentType)
+	io.Copy(w, f)
+	return nil
+}
+
+func notFoundHanler(w http.ResponseWriter, r *http.Request) {
+	// まずリクエストされた通りにファイルを探索
+	err := tryRead(r.URL.Path, w)
+	if err != nil {
+		return
+	}
+	// 見つからなければindex.htmlを返す
+	err = tryRead("index.html", w)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func NewHandler() http.Handler {
+	router := chi.NewRouter()
+
+	router.Route("/api", func(r chi.Router) {
+		r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+		})
+	})
+
+	// シングルページアプリケーションを配布するハンドラー
+	router.NotFound(notFoundHanler)
+
+	return router
+}
--- main.go
+++ main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
-	"github.com/HarukiIdo/go_sample/db"
-	"github.com/HarukiIdo/go_sample/router"
+	"github.com/HarukiIdo/go_sample/handler"
 )
 
 func main() {
 
-	db, err := db.NewDB("/")
-	if err != nil {
-		return
-	}
-	mux := router.NewRouter(db)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
 
-	http.ListenAndServe(":8080", mux)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: handler.NewHandler(),
+	}
+
+	go func() {
+		<-ctx.Done()
+		ctx, cancel := context.WithTimeout(context.Background(), 5+time.Second)
+		defer cancel()
+		server.Shutdown(ctx)
+	}()
+	fmt.Printf("start receiving at :8080")
+	fmt.Fprintln(os.Stderr, server.ListenAndServe())
 }
